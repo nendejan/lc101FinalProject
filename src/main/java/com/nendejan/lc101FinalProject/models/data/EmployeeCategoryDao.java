5a7e85415a3e925dca3ba105ac1b98bca259b205
nico <nendejan@gmail.com>
2017-12-03 12:27:03 -0600

    removal or employee catagories now functioning correctly

--- src/main/java/com/nendejan/lc101FinalProject/models/data/EmployeeCategoryDao.java
+++ src/main/java/com/nendejan/lc101FinalProject/models/data/EmployeeCategoryDao.java
@@ -1,6 +1,7 @@
 package com.nendejan.lc101FinalProject.models.data;
 
 import com.nendejan.lc101FinalProject.models.EmployeeCategory;
+import com.nendejan.lc101FinalProject.models.workplace;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,8 @@ import javax.transaction.Transactional;
  */
 @Repository
 @Transactional
-public interface EmployeeCategoryDao extends CrudRepository<EmployeeCategory, Integer> {}
+public interface EmployeeCategoryDao extends CrudRepository<EmployeeCategory, Integer> {
+/*todo: find syntax for creation of custom dao methods*/
+    EmployeeCategory findByName(String name);
+    EmployeeCategory findByNameAndWorkplace(String name, workplace workplace);
+}
